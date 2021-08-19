00494d85d20fb784f40d04058d87609306bd3631
Asuka-Hongo <75460862+araaiko@users.noreply.github.com>
2021-08-19 17:56:26 +0900

    HTML準備

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
 import "./styles.css";
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      {/* 入力欄 */}
+      <div>
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+
+      {/* 未完了TODO */}
+      <div>
+        <p>未完了のTODO</p>
+        <ul>
+          <li>
+            <div>あああああああ</div>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+          <li>
+            <div>いいいいいいい</div>
+            <button>完了</button>
+            <button>削除</button>
+          </li>
+        </ul>
+      </div>
+
+      {/* 完了TODO */}
+      <div>
+        <p>完了のTODO</p>
+        <ul>
+          <li>
+            <div>ううううう</div>
+            <button>戻す</button>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
